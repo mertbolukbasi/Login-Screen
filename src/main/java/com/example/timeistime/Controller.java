4afe405665af69768021f00dfe21271b3853424e
Mert <bolukbasmert@hotmail.com>
2024-10-21 12:47:20 +0300

    isimler ayarlandı

--- src/main/java/com/example/timeistime/Controller.java
+++ src/main/java/com/example/timeistime/Controller.java
@@ -3,7 +3,7 @@ package com.example.timeistime;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
